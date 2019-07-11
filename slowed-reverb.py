3b5d3f7bf6672230e48f867ed0a0979f26d7b9db
Jérémy <jeremyschneider21@gmail.com>
2019-07-11 23:26:59 +0200

    Update design and replace whitespaces by an underscore

--- slowed-reverb.py
+++ slowed-reverb.py
@@ -9,45 +9,47 @@ class App:
         self.app = app
         self.app.title("slowed+reverb")
         self.app.iconbitmap("slowed_reverb.ico")
-        self.app.minsize(width=540, height=360)
-
+        self.app.minsize(width=640, height=500)
+        self.app.maxsize(width=640, height=500)
+        self.app.configure(background="#6622b8")
+        
         self.top_frame = Frame(self.app)
         self.top_frame.pack(padx=20, pady=20)
         self.middle_frame = Frame(self.app)
-        self.middle_frame.pack(padx=20, pady=20)
+        self.middle_frame.pack(padx=10, pady=10, expand=1)
         self.bottom_frame = Frame(self.app)
-        self.bottom_frame.pack(padx=20, pady=20)
+        self.bottom_frame.pack(expand=1)
 
         self.filepath = ""
         self.filename = StringVar(value="No file selected")
         self.export_path = "exported_files"
 
         #Top frame
-        self.selected_file = Label(self.top_frame, textvariable=self.filename)
+        self.selected_file = Label(self.top_frame, textvariable=self.filename, fg="#6622b8")
         self.selected_file.pack(side="left")
 
-        self.select_file_button = Button(self.top_frame, text="Select file", command=self.select_file)
+        self.select_file_button = Button(self.top_frame, text="Select a file", command=self.select_file, fg="#6622b8")
         self.select_file_button.pack(side="left")
         
         #Middle frame
-        self.slowdown = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, label="framerate:")
-        self.slowdown.pack(side="left")
+        self.slowdown = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, width="10", label="framerate:")
+        self.slowdown.pack(side="top")
 
-        self.reverberance = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, label="reverberance:")
-        self.reverberance.pack(side="left")
+        self.reverberance = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, width="10", label="reverberance:")
+        self.reverberance.pack(side="top")
 
-        self.hf_damping = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, label="hf damping:")
-        self.hf_damping.pack(side="left")
+        self.hf_damping = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, width="10", label="hf damping:")
+        self.hf_damping.pack(side="top")
 
-        self.room_scale = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, label="room scale:")
-        self.room_scale.pack(side="left")
+        self.room_scale = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, width="10", label="room scale:")
+        self.room_scale.pack(side="top")
 
-        self.pre_delay = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, label="pre delay:")
-        self.pre_delay.pack(side="left")
+        self.pre_delay = Scale(self.middle_frame, orient="horizontal", from_=0, to=100, width="10", label="pre delay:")
+        self.pre_delay.pack(side="top")
 
         #Bottom frame
-        self.select_file_button = Button(self.bottom_frame, text="slowed+reverb", command=self.slowed_reverb)
-        self.select_file_button.pack()
+        self.select_file_button = Button(self.bottom_frame, text="slowed+reverb", command=self.slowed_reverb, fg="#6622b8")
+        self.select_file_button.pack(side="bottom")
         
     def select_file(self):
         filepath = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=(("wav files", "*.wav"), ("mp3 files", "*.mp3")))
@@ -62,7 +64,7 @@ class App:
         slowed.set_frame_rate(audio.frame_rate)
 
         with tempfile.TemporaryDirectory() as tmpwav:
-            slowed.export(f"{tmpwav}\{self.filename.get()}.wav", format="wav")
+            slowed.export(f"{tmpwav}\export.wav", format="wav")
                 
             os.makedirs(os.path.dirname('exported_files/'), exist_ok=True)
 
@@ -72,7 +74,8 @@ class App:
                 room_scale=self.room_scale.get(),
                 pre_delay=self.pre_delay.get()
                 )
-            fx(f"{tmpwav}\{self.filename.get()}.wav", f"{self.export_path}/{os.path.splitext(self.filename.get())[0]}.wav")
+            export_filename = os.path.splitext(self.filename.get())[0].replace(" ", "_")
+            fx(f"{tmpwav}\export.wav", f"{self.export_path}/{export_filename}.wav")
 
 if __name__ == "__main__":
     init = Tk()
